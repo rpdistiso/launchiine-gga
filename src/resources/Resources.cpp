7309ffec07a4946522fbb162d67e4a30cca6ef2d
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-08-27 12:32:05 -0400

    Optimize Resources.cpp

--- src/resources/Resources.cpp
+++ src/resources/Resources.cpp
@@ -7,7 +7,6 @@
 #include <malloc.h>
 #include <string.h>
 #include <string>
-
 #include <chrono>
 #include <future>
 #include <iostream>
@@ -15,156 +14,156 @@
 #include <thread>
 
 
-Resources *Resources::instance = nullptr;
+Resources * Resources::instance = nullptr;
 
 void Resources::Clear() {
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        if (RecourceList[i].CustomFile) {
-            free(RecourceList[i].CustomFile);
-            RecourceList[i].CustomFile = nullptr;
-        }
-
-        if (RecourceList[i].CustomFileSize != 0)
-            RecourceList[i].CustomFileSize = 0;
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    if (RecourceList[i].CustomFile) {
+      free(RecourceList[i].CustomFile);
+      RecourceList[i].CustomFile = nullptr;
     }
 
-    if (instance)
-        delete instance;
+    if (RecourceList[i].CustomFileSize != 0)
+      RecourceList[i].CustomFileSize = 0;
+  }
 
-    instance = nullptr;
+  if (instance)
+    delete instance;
+
+  instance = nullptr;
 }
 
-bool Resources::LoadFiles(const char *path) {
-    if (!path)
-        return false;
+bool Resources::LoadFiles(const char * path) {
+  if (!path)
+    return false;
 
-    bool result = false;
-    Clear();
+  bool result = false;
+  Clear();
 
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        std::string fullpath(path);
-        fullpath += "/";
-        fullpath += RecourceList[i].filename;
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    std::string fullpath(path);
+    fullpath += "/";
+    fullpath += RecourceList[i].filename;
 
-        uint8_t *buffer   = nullptr;
-        uint32_t filesize = 0;
+    uint8_t * buffer = nullptr;
+    uint32_t filesize = 0;
 
-        FSUtils::LoadFileToMem(fullpath.c_str(), &buffer, &filesize);
+    FSUtils::LoadFileToMem(fullpath.c_str(), & buffer, & filesize);
 
-        RecourceList[i].CustomFile     = buffer;
-        RecourceList[i].CustomFileSize = (uint32_t) filesize;
-        result |= (buffer != 0);
-    }
+    RecourceList[i].CustomFile = buffer;
+    RecourceList[i].CustomFileSize = (uint32_t) filesize;
+    result |= (buffer != 0);
+  }
 
-    return result;
+  return result;
 }
 
-const uint8_t *Resources::GetFile(const char *filename) {
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        if (strcasecmp(filename, RecourceList[i].filename) == 0) {
-            return (RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile);
-        }
+const uint8_t * Resources::GetFile(const char * filename) {
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    if (strcasecmp(filename, RecourceList[i].filename) == 0) {
+      return (RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile);
     }
+  }
 
-    return nullptr;
+  return nullptr;
 }
 
-uint32_t Resources::GetFileSize(const char *filename) {
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        if (strcasecmp(filename, RecourceList[i].filename) == 0) {
-            return (RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize);
-        }
+uint32_t Resources::GetFileSize(const char * filename) {
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    if (strcasecmp(filename, RecourceList[i].filename) == 0) {
+      return (RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize);
     }
-    return 0;
+  }
+  return 0;
 }
 
-GuiImageData *Resources::GetImageData(const char *filename) {
-    if (!instance)
-        instance = new Resources;
+GuiImageData * Resources::GetImageData(const char * filename) {
+  if (!instance)
+    instance = new Resources;
 
-    std::map<std::string, std::pair<uint32_t, GuiImageData *>>::iterator itr = instance->imageDataMap.find(std::string(filename));
-    if (itr != instance->imageDataMap.end()) {
-        itr->second.first++;
-        return itr->second.second;
+  std::map < std::string, std::pair < uint32_t, GuiImageData * >> ::iterator itr = instance -> imageDataMap.find(std::string(filename));
+  if (itr != instance -> imageDataMap.end()) {
+    itr -> second.first++;
+    return itr -> second.second;
+  }
+
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    if (strcasecmp(filename, RecourceList[i].filename) == 0) {
+      const uint8_t * buff = RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile;
+      const uint32_t size = RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize;
+
+      if (buff == nullptr)
+        return nullptr;
+
+      GuiImageData * image = new GuiImageData(buff, size);
+      instance -> imageDataMap[std::string(filename)].first = 1;
+      instance -> imageDataMap[std::string(filename)].second = image;
+
+      return image;
     }
+  }
 
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        if (strcasecmp(filename, RecourceList[i].filename) == 0) {
-            const uint8_t *buff = RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile;
-            const uint32_t size = RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize;
-
-            if (buff == nullptr)
-                return nullptr;
-
-            GuiImageData *image                                  = new GuiImageData(buff, size);
-            instance->imageDataMap[std::string(filename)].first  = 1;
-            instance->imageDataMap[std::string(filename)].second = image;
-
-            return image;
-        }
-    }
-
-    return nullptr;
+  return nullptr;
 }
 
-void Resources::RemoveImageData(GuiImageData *image) {
-    std::map<std::string, std::pair<uint32_t, GuiImageData *>>::iterator itr;
+void Resources::RemoveImageData(GuiImageData * image) {
+  std::map < std::string, std::pair < uint32_t, GuiImageData * >> ::iterator itr;
 
-    for (itr = instance->imageDataMap.begin(); itr != instance->imageDataMap.end(); itr++) {
-        if (itr->second.second == image) {
-            itr->second.first--;
+  for (itr = instance -> imageDataMap.begin(); itr != instance -> imageDataMap.end(); itr++) {
+    if (itr -> second.second == image) {
+      itr -> second.first--;
 
-            if (itr->second.first == 0) {
-                AsyncExecutor::pushForDelete(itr->second.second);
+      if (itr -> second.first == 0) {
+        AsyncExecutor::pushForDelete(itr -> second.second);
 
-                instance->imageDataMap.erase(itr);
-            }
-            break;
-        }
+        instance -> imageDataMap.erase(itr);
+      }
+      break;
     }
+  }
 }
 
-GuiSound *Resources::GetSound(const char *filename) {
-    if (!instance)
-        instance = new Resources;
+GuiSound * Resources::GetSound(const char * filename) {
+  if (!instance)
+    instance = new Resources;
 
-    std::map<std::string, std::pair<uint32_t, GuiSound *>>::iterator itr = instance->soundDataMap.find(std::string(filename));
-    if (itr != instance->soundDataMap.end()) {
-        itr->second.first++;
-        return itr->second.second;
+  std::map < std::string, std::pair < uint32_t, GuiSound * >> ::iterator itr = instance -> soundDataMap.find(std::string(filename));
+  if (itr != instance -> soundDataMap.end()) {
+    itr -> second.first++;
+    return itr -> second.second;
+  }
+
+  for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
+    if (strcasecmp(filename, RecourceList[i].filename) == 0) {
+      const uint8_t * buff = RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile;
+      const uint32_t size = RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize;
+
+      if (buff == nullptr)
+        return nullptr;
+
+      GuiSound * sound = new GuiSound(buff, size);
+      instance -> soundDataMap[std::string(filename)].first = 1;
+      instance -> soundDataMap[std::string(filename)].second = sound;
+
+      return sound;
     }
+  }
 
-    for (int32_t i = 0; RecourceList[i].filename != nullptr; ++i) {
-        if (strcasecmp(filename, RecourceList[i].filename) == 0) {
-            const uint8_t *buff = RecourceList[i].CustomFile ? RecourceList[i].CustomFile : RecourceList[i].DefaultFile;
-            const uint32_t size = RecourceList[i].CustomFile ? RecourceList[i].CustomFileSize : RecourceList[i].DefaultFileSize;
-
-            if (buff == nullptr)
-                return nullptr;
-
-            GuiSound *sound                                      = new GuiSound(buff, size);
-            instance->soundDataMap[std::string(filename)].first  = 1;
-            instance->soundDataMap[std::string(filename)].second = sound;
-
-            return sound;
-        }
-    }
-
-    return nullptr;
+  return nullptr;
 }
 
-void Resources::RemoveSound(GuiSound *sound) {
-    std::map<std::string, std::pair<uint32_t, GuiSound *>>::iterator itr;
+void Resources::RemoveSound(GuiSound * sound) {
+  std::map < std::string, std::pair < uint32_t, GuiSound * >> ::iterator itr;
 
-    for (itr = instance->soundDataMap.begin(); itr != instance->soundDataMap.end(); itr++) {
-        if (itr->second.second == sound) {
-            itr->second.first--;
+  for (itr = instance -> soundDataMap.begin(); itr != instance -> soundDataMap.end(); itr++) {
+    if (itr -> second.second == sound) {
+      itr -> second.first--;
 
-            if (itr->second.first == 0) {
-                AsyncExecutor::pushForDelete(itr->second.second);
-                instance->soundDataMap.erase(itr);
-            }
-            break;
-        }
+      if (itr -> second.first == 0) {
+        AsyncExecutor::pushForDelete(itr -> second.second);
+        instance -> soundDataMap.erase(itr);
+      }
+      break;
     }
+  }
 }
