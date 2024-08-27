ebf9fe61abfffcb08b603eb4e3017ba69e4a910e
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-08-27 12:04:15 -0400

    Optimize entry.cpp

--- src/entry.cpp
+++ src/entry.cpp
@@ -5,35 +5,36 @@
 #include <whb/log_module.h>
 #include <whb/log_udp.h>
 
-int32_t main(int32_t argc, char **argv) {
-    bool moduleInit;
-    bool cafeInit = false;
-    bool udpInit  = false;
+int32_t main(int32_t argc, char ** argv) {
+  bool moduleInit;
+  bool cafeInit = false;
+  bool udpInit = false;
 
-    if (!(moduleInit = WHBLogModuleInit())) {
-        cafeInit = WHBLogCafeInit();
-        udpInit  = WHBLogUdpInit();
-    }
-    DEBUG_FUNCTION_LINE("Starting launchiine " LAUNCHIINE_VERSION "");
+  if (!(moduleInit = WHBLogModuleInit())) {
+    cafeInit = WHBLogCafeInit();
+    udpInit = WHBLogUdpInit();
+  }
+  DEBUG_FUNCTION_LINE("Starting launchiine "
+    LAUNCHIINE_VERSION "");
 
-    DEBUG_FUNCTION_LINE("Start main application");
-    Application::instance()->exec();
+  DEBUG_FUNCTION_LINE("Start main application");
+  Application::instance() -> exec();
 
-    DEBUG_FUNCTION_LINE("Main application stopped");
-    Application::destroyInstance();
+  DEBUG_FUNCTION_LINE("Main application stopped");
+  Application::destroyInstance();
 
-    DEBUG_FUNCTION_LINE("Peace out...");
+  DEBUG_FUNCTION_LINE("Peace out...");
 
-    if (cafeInit) {
-        WHBLogCafeDeinit();
-    }
+  if (cafeInit) {
+    WHBLogCafeDeinit();
+  }
 
-    if (udpInit) {
-        WHBLogUdpDeinit();
-    }
+  if (udpInit) {
+    WHBLogUdpDeinit();
+  }
 
-    if (moduleInit) {
-        WHBLogModuleDeinit();
-    }
-    return 0;
+  if (moduleInit) {
+    WHBLogModuleDeinit();
+  }
+  return 0;
 }
