85f1907e67b3cc60f5c923ff1ef713ba361fc9a8
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-03 10:43:54 -0400

    Update GuiController.h

--- src/libgui/include/gui/GuiController.h
+++ src/libgui/include/gui/GuiController.h
@@ -1,23 +1,7 @@
-/****************************************************************************
- * Copyright (C) 2015 Dimok
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ****************************************************************************/
 #ifndef GUI_CONTROLLER_H_
 #define GUI_CONTROLLER_H_
 
-#include <gui/GuiTrigger.h>
+#include "gui/GuiTrigger.h"
 #include <string.h>
 
 class GuiController {
