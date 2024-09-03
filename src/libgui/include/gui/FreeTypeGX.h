5fb18c10575d8bd64700822a42d290d3074e7b0b
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-03 10:37:27 -0400

    Update FreeTypeGX.h

--- src/libgui/include/gui/FreeTypeGX.h
+++ src/libgui/include/gui/FreeTypeGX.h
@@ -1,25 +1,3 @@
-/*
- * FreeTypeGX is a wrapper class for libFreeType which renders a compiled
- * FreeType parsable font into a GX texture for Wii homebrew development.
- * Copyright (C) 2008 Armin Tamzarian
- * Modified by Dimok, 2015 for WiiU GX2
- *
- * This file is part of FreeTypeGX.
- *
- * FreeTypeGX is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * FreeTypeGX is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with FreeTypeGX.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include <ft2build.h>
@@ -34,7 +12,7 @@
 #include <wchar.h>
 
 
-#include <gui/gx2_ext.h>
+#include "gui/gx2_ext.h"
 
 #include <gx2/sampler.h>
 #include <gx2/texture.h>
