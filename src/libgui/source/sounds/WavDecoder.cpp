af6770975251b987a63f00e6af69a4697202b915
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-03 10:27:40 -0400

    Update WavDecoder.cpp

--- src/libgui/source/sounds/WavDecoder.cpp
+++ src/libgui/source/sounds/WavDecoder.cpp
@@ -14,10 +14,8 @@
  * must not claim that you wrote the original software. If you use
  * this software in a product, an acknowledgment in the product
  * documentation would be appreciated but is not required.
- *
  * 2. Altered source versions must be plainly marked as such, and
  * must not be misrepresented as being the original software.
- *
  * 3. This notice may not be removed or altered from any source
  * distribution.
  *
@@ -25,7 +23,7 @@
  ***************************************************************************/
 #include "fs/CFile.hpp"
 #include "utils/utils.h"
-#include <gui/sounds/WavDecoder.hpp>
+#include "gui/sounds/WavDecoder.hpp"
 #include <string.h>
 
 WavDecoder::WavDecoder(const char *filepath)
