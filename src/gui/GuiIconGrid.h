12460dc8ec331edf36cd569592eb66110d6fbfe4
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-03 08:43:33 -0400

    Use local libgui

--- src/gui/GuiIconGrid.h
+++ src/gui/GuiIconGrid.h
@@ -1,27 +1,11 @@
-/****************************************************************************
- * Copyright (C) 2015 Dimok
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ****************************************************************************/
 #pragma once
 
 #include "gui/GameIcon.h"
 #include "gui/GuiDragListener.h"
 #include "gui/GuiTitleBrowser.h"
+#include "gui/GuiParticleImage.h"
 #include "utils/AsyncExecutor.h"
 #include "utils/logger.h"
-#include <gui/GuiParticleImage.h>
 #include <map>
 
 class GuiIconGrid : public GuiTitleBrowser, public sigslot::has_slots<> {
