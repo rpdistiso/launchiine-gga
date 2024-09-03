9cd1924dcebfc3ded7d0d7f8eff0427a93017a76
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-03 08:16:18 -0400

    Use local libgui

--- src/gui/GuiDragListener.h
+++ src/gui/GuiDragListener.h
@@ -1,23 +1,10 @@
-/****************************************************************************
- * Copyright (C) 2016 Maschell
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ****************************************************************************/
-#ifndef GUI_DRAG_LISTENER_H_
-#define GUI_DRAG_LISTENER_H_
+#ifndef GUI_DRAG_LISTENER_H
+#define GUI_DRAG_LISTENER_H
 
-#include <gui/GuiButton.h>
-#include <gui/GuiController.h>
-#include <gui/GuiElement.h>
-#include <gui/GuiTrigger.h>
+#include "gui/GuiButton.h"
+#include "gui/GuiController.h"
+#include "gui/GuiElement.h"
+#include "gui/GuiTrigger.h"
 
 class GuiDragListener : public GuiElement {
 public:
