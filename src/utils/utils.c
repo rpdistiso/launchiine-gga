c8c04ce6253d98d5ea4acaf30cde80dd9830bd5f
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-09-05 14:21:29 -0400

    Revise utils.c

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <utils/logger.h>
+#include "utils/logger.h"
 #include <sys/stat.h>
 
 // https://gist.github.com/ccbrown/9722406
